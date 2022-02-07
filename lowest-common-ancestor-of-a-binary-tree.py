bc3e6e39cab716d1593475442157f989170189a6
dhtmaks2540 <gjsxj725@daum.net>
2022-02-07 10:49:52 +0900

    스택(DFS)과 딕셔너리를 사용하여 접근하는 방법 추가

--- lowest-common-ancestor-of-a-binary-tree.py
+++ lowest-common-ancestor-of-a-binary-tree.py
@@ -26,8 +26,6 @@ class Solution:
                 if node.right:
                     queue.append((node.right, path + [node.right.val]))
 
-        print("done!!")
-
         for i in range(len(result[0])):
             if result[0][i] != result[1][i]:
                 return result[0][i - 1]
@@ -56,6 +54,71 @@ class Solution:
         # if not left and not right: return None
         # return left or right
         return root if left and right else left or right
+
+"""
+https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/solution/
+
+각 노드의 parent pointers를 갖고 있다면, p와 q의 조상을 얻기 위한 백트래킹을 수행할 수 있다.
+
+이 과정동안 얻는 첫 번째 공통 노드가 LCA 노드일 것이다. 트리를 traverse할 때, 부모 포인터를 딕셔너리에 저장한다.
+
+1. root node에서 시작하여 트리를 traverse
+2. p,q를 찾기 전까지 딕셔너리에 부모 포인터를 저장
+3. p와 q를 모두 찾으면, 부모 딕셔너리를 사용해 p의 모든 조상을 구하고 ancestors라는 set에 추가
+4. 동일하게 q에 대해 조상을 traverse한다. p의 ancestors 집합에 조상이 존재한다면, 이것은 
+p와 q 사이에 공통되는 첫 번째 조상임을 의미하고 따라서 이것이 LCA 노드
+"""
+
+class Solution:
+
+    def lowestCommonAncestor(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+
+        # Stack for tree traversal
+        # 트리 순회를 위한 Stack
+        stack = [root]
+
+        # Dictionary for parent pointers
+        # parent pointers(부모 노드를 가리키는 포인터)를 위한 딕셔너리
+        parent = {root: None}
+
+        # Iterate until we find both the nodes p and q
+        # p와 q 모두를 찾기 전까지 반복
+        while p not in parent or q not in parent:
+            node = stack.pop()
+
+            # While traversing the tree, keep saving the parent pointers.
+            # 트리를 순회하는 동안에, parent pointer를 계속해서 저장
+            if node.left:
+                # node.left의 부모는 node(현재 노드)
+                parent[node.left] = node
+                stack.append(node.left)
+            if node.right:
+                # node.right의 부모는 node(현재 노드)
+                parent[node.right] = node
+                stack.append(node.right)
+
+        # Ancestors set() for node p.
+        # p를 위한 조상 집합
+        ancestors = set()
+
+        # Process all ancestors for node p using parent pointers.
+        # parent pointer를 사용하여 노드 p의 모든 조상을 처리
+        while p:
+            ancestors.add(p)
+            p = parent[p]
+
+        # The first ancestor of q which appears in
+        # p's ancestor set() is their lowest common ancestor.
+        # p의 조상 집합 안에서 나타난 q의 첫 번째 조상이 그들의 lca이다.
+        while q not in ancestors:
+            q = parent[q]
+        return q
 
 root = TreeNode(3)
 left = TreeNode(5)
