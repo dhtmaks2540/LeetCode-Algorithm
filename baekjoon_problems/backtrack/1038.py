03f2ce61bffc74dddeb4c03535ba4defd2ab9b9e
dhtmaks2540 <gjsxj725@daum.net>
2022-04-04 12:04:17 +0900

    백트래킹을 사용하여 문제 풀이

--- baekjoon_problems/backtrack/1038.py
+++ baekjoon_problems/backtrack/1038.py
@@ -1,6 +1,8 @@
 """
 음이 아닌 정수 X의 자릿수가 가장 큰 자릿수부터 가장 작은 자릿수까지 감소한다면 그 수를 감소하는
 수라고 한다. 이때, N이 주어질 경우 N번째 감소하는 수를 출력하라.
+
+https://khsung0.tistory.com/22
 """
 
 # 시간초과
@@ -33,15 +35,29 @@ def brute_force():
             exit()
 
 
-N = int(input())
-numbers = [0] * 10
-
-# 예외처리
-if N <= 9:
-    print(N)
-    exit()
-
-N -= 9
-
-def backtracking():
-    pass
+# backtracking을 사용하여 문제 풀이
+def solution():
+    def add_digit(digit, num): # 자릿수에 따라 증가
+        # 탈출 조건
+        if digit == 1:
+            decreasing.append(num)
+        else:
+            for i in range(num % 10):
+                # digit 감소 후 재귀 호출
+                add_digit(digit - 1, num * 10 + i)
+                
+    def backtracking(digit): # 백트래킹 시작
+        for i in range(digit - 1, 10):
+            add_digit(digit, i)
+            
+    decreasing = [] # 감소하는 숫자 리스트
+    for i in range(1, 11):
+        backtracking(i)
+        
+    n = int(input())
+    if n >= len(decreasing): # 감소하는 숫자가 없을 때
+        print(-1)
+    else:
+        print(decreasing[n])
+        
+solution()
